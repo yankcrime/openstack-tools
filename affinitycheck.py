90fdc3807eaf5733ac5bb6346c11fa7aa1cfa0f7
Nick Jones <nick.jones@datacentred.co.uk>
2016-04-09 17:58:39 +0100

    Refactoring, handling of additional cases
    This commit:
    
    * Slight refactoring / tidying
    * Checks for empty / invalid (i.e no anti-affinity rules) groups

--- affinitycheck.py
+++ affinitycheck.py
@@ -1,77 +1,94 @@
 #!/usr/bin/env python
+""" Utility to check validity of anti-affinity rules """
 
 import os
 import sys
 import argparse
-import prettytable
-from operator import itemgetter, attrgetter
 from collections import defaultdict, Counter
 from keystoneclient import session
 from keystoneclient.auth.identity import v2
 from keystoneclient.v2_0 import client as ksclient
 from novaclient import client as nclient
+import prettytable
 
 def get_credentials():
-    d = {}
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['username'] = os.environ['OS_USERNAME']
-    d['password'] = os.environ['OS_PASSWORD']
-    d['tenant_name'] = os.environ['OS_TENANT_NAME']
-    return d
-
-credentials = get_credentials()
-keystone = ksclient.Client(**credentials)
-auth = v2.Password(**credentials)
-sess = session.Session(auth=auth)
-nova = nclient.Client(2, session=sess)
+    """ Build dictionary of Keystone credentials from environment """
+    keystone_credentials = {}
+    keystone_credentials['auth_url'] = os.environ['OS_AUTH_URL']
+    keystone_credentials['username'] = os.environ['OS_USERNAME']
+    keystone_credentials['password'] = os.environ['OS_PASSWORD']
+    keystone_credentials['tenant_name'] = os.environ['OS_TENANT_NAME']
+    return keystone_credentials
 
 def get_args():
+    """ Get commandline arguments """
     parser = argparse.ArgumentParser(description="Affinity rule checker")
     parser.add_argument('--check', type=str)
     parser.add_argument('--list', type=str)
     return parser.parse_args()
 
 def get_server(serverid):
+    """ Return Server object """
     return nova.servers.get(serverid)
 
-def get_group_members(groupid):
-    server_group = nova.server_groups.get(groupid)
-    return server_group.members
+def get_group_members(server_group_id):
+    """ Return list of instance UUIDs present in a Server Group """
+    server_group = nova.server_groups.get(server_group_id)
+    if 'anti-affinity' in server_group.policies:
+        return server_group.members
+    else:
+        return False
 
 def create_table(fields):
-    pt = prettytable.PrettyTable(fields, caching=False)
-    pt.align = 'l'
-    return pt
+    """ Boilerplate for PrettyTable """
+    table = prettytable.PrettyTable(fields, caching=False)
+    table.align = 'l'
+    return table
 
 def print_group_members(server_group_id):
-    table = create_table(['Instance ID', 'Instance', 'Hypervisor'])
-    for server in get_group_members(server_group_id):
-        instance = get_server(server)
-        hypervisor = getattr(instance, 'OS-EXT-SRV-ATTR:hypervisor_hostname'.split('.')[0])
-        table.add_row([instance.id, instance.name, hypervisor])
-    print table
-
-def print_group_duplicates(server_group_id):
-    hypervisors = []
-    instances = defaultdict(list)
-    for instance in get_group_members(server_group_id):
-        i = get_server(instance)
-        h = getattr(i, 'OS-EXT-SRV-ATTR:hypervisor_hostname')
-        instances[instance].append(i.name)
-        instances[instance].append(h)
-        hypervisors.append(h)
-    dupes = [k for k, v in Counter(hypervisors).items() if v > 1]
-    if dupes:
-        print "Anti-affinity rules violated in Server Group:", server_group_id
+    """ Print a table detailing Server Group instances and their hypervisors """
+    group_members = get_group_members(server_group_id)
+    if group_members:
         table = create_table(['Instance ID', 'Instance', 'Hypervisor'])
-        for instance_id, [instance_name, hypervisor] in instances.items():
-            if hypervisor in dupes:
-                table.add_row([instance_id, instance_name, hypervisor])
+        for server in get_group_members(server_group_id):
+            instance = get_server(server)
+            hypervisor = getattr(instance, 'OS-EXT-SRV-ATTR:hypervisor_hostname'.split('.')[0])
+            table.add_row([instance.id, instance.name, hypervisor])
         print table
     else:
-        print "No anti-affinity rules violated for Server Group:", server_group_id
+        print "Server Group", server_group_id, "empty or does not have an anti-affinity policy set."
+
+def print_group_duplicates(server_group_id):
+    """ Evaluate whether any instances in a SG have been scheduled to the same hypervisor """
+    group_members = get_group_members(server_group_id)
+    if group_members:
+        hypervisors = []
+        instances = defaultdict(list)
+        for instance in get_group_members(server_group_id):
+            i = get_server(instance)
+            hypervisor = getattr(i, 'OS-EXT-SRV-ATTR:hypervisor_hostname')
+            instances[instance].append(i.name)
+            instances[instance].append(hypervisor)
+            hypervisors.append(hypervisor)
+        dupes = [k for k, v in Counter(hypervisors).items() if v > 1]
+        if dupes:
+            print "Anti-affinity rules violated in Server Group:", server_group_id
+            table = create_table(['Instance ID', 'Instance', 'Hypervisor'])
+            [table.add_row([instance_id, instance_name, hypervisor])
+             for instance_id, [instance_name, hypervisor] in instances.items()
+             if hypervisor in dupes]
+            print table
+        else:
+            print "No anti-affinity rules violated for Server Group:", server_group_id
+    else:
+        print "Server Group", server_group_id, "empty or does not have an anti-affinity policy set."
 
 if __name__ == '__main__':
+    credentials = get_credentials()
+    keystone = ksclient.Client(**credentials)
+    auth = v2.Password(**credentials)
+    sess = session.Session(auth=auth)
+    nova = nclient.Client(2, session=sess)
     args = get_args()
     group = sys.argv[2]
     if args.check:
